c4873f53d80d73bcab3a522ebaa8567672a4273f
mistelltein <140878066+mistelltein@users.noreply.github.com>
2024-01-27 20:26:19 +0600

    Adding сard-functionality

--- ANC-MVC-BookShoop/Repositpries/CartRepository.cs
+++ ANC-MVC-BookShoop/Repositpries/CartRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
@@ -17,15 +18,17 @@ namespace ANC_MVC_BookShoop.Repositpries
 			_userManager = userManager;
 			_httpContextAccessor = httpContextAccessor;
 		}
-        public async Task<bool> AddItem(int bookId, int quantity)
+        public async Task<int> AddItem(int bookId, int quantity)
 		{
+			string userId = GetUserId();
 			using var transaction = _db.Database.BeginTransaction();
 
 			try
 			{
-				string userId = GetUserId();
 				if (string.IsNullOrEmpty(userId))
-					return false;
+				{
+					throw new Exception("Invalid User-Id");
+				}
 				var cart = await GetCart(userId);
 
 				if (cart is null)
@@ -33,14 +36,14 @@ namespace ANC_MVC_BookShoop.Repositpries
 					cart = new ShoppingCart
 					{
 						UserId = userId
-
 					};
 					_db.ShoppingCarts.Add(cart);
 				}
 
 				_db.SaveChanges();
 
-				var cartItem = _db.CartDetails.FirstOrDefault(a => a.ShoppingCartId == cart.Id && a.BookId == bookId);
+				var cartItem = _db.CartDetails
+					.FirstOrDefault(a => a.ShoppingCartId == cart.Id && a.BookId == bookId);
 
 				if (cartItem != null )
 				{
@@ -58,32 +61,38 @@ namespace ANC_MVC_BookShoop.Repositpries
 				}
 				_db.SaveChanges();
 				transaction.Commit();
-				return true;
 			}
 			catch (Exception ex)
 			{
-				return false;
 			}
+			var cartItemCount = await GetCartItemCount(userId);
+			return cartItemCount;
 		}
 
-		public async Task<bool> RemoveItem(int bookId)
+		public async Task<int> RemoveItem(int bookId)
 		{
+			string userId = GetUserId();
 			try
 			{
-				string userId = GetUserId();
 				if (string.IsNullOrEmpty(userId))
-					return false;
+				{
+					throw new Exception("Invalid User-Id");
+				}
+
 				var cart = await GetCart(userId);
 
 				if (cart is null)
 				{
-					return false;
+					throw new Exception("Invalid cart");
 				}
 
-				var cartItem = _db.CartDetails.FirstOrDefault(a => a.ShoppingCartId == cart.Id && a.BookId == bookId);
+				var cartItem = _db.CartDetails
+					.FirstOrDefault(a => a.ShoppingCartId == cart.Id && a.BookId == bookId);
 
 				if (cartItem is null)
-					return false;
+				{
+					throw new Exception("No items in cart");
+				}		
 				else if (cartItem.Quantity == 1)
 				{
 					_db.CartDetails.Remove(cartItem);
@@ -93,15 +102,15 @@ namespace ANC_MVC_BookShoop.Repositpries
 					cartItem.Quantity = cartItem.Quantity - 1;
 				}
 				_db.SaveChanges();
-				return true;
 			}
 			catch (Exception ex)
 			{
-				return false;
 			}
+			var cartItemCount = await GetCartItemCount(userId);
+			return cartItemCount;
 		}
 
-		public async Task<IEnumerable<ShoppingCart>> GetUserCart()
+		public async Task<ShoppingCart> GetUserCart()
 		{
 			var userId = GetUserId();
 
@@ -112,15 +121,31 @@ namespace ANC_MVC_BookShoop.Repositpries
 				.Include(a => a.CartDetails)
 				.ThenInclude(a => a.Book)
 				.ThenInclude(a => a.Genre)
-				.Where(a => a.UserId == userId).ToListAsync();
+				.Where(a => a.UserId == userId).FirstOrDefaultAsync();
 
 			return shoppingCart;
 		}
 
-		private async Task<ShoppingCart> GetCart(string userId)
+		public async Task<ShoppingCart> GetCart(string userId)
 		{
-			var cart = await _db.ShoppingCarts.FirstOrDefaultAsync(x => x.UserId == userId);
+			var cart = await _db.ShoppingCarts
+				.FirstOrDefaultAsync(x => x.UserId == userId);
 			return cart;
+		}
+
+		public async Task<int> GetCartItemCount(string userId = "")
+		{
+			if (string.IsNullOrEmpty(userId))
+			{
+				userId = GetUserId();
+			}
+			var data = await (from cart in _db.ShoppingCarts
+						join cartDetail in _db.CartDetails
+						on cart.Id equals cartDetail.ShoppingCartId
+						select new { cartDetail.Id }
+						).ToListAsync();
+
+			return data.Count;
 		}
 
 		private string GetUserId()
--- ANC-MVC-BookShoop/Repositpries/ICartRepository.cs
+++ ANC-MVC-BookShoop/Repositpries/ICartRepository.cs
@@ -2,8 +2,10 @@
 {
 	public interface ICartRepository
 	{
-		Task<bool> AddItem(int bookId, int quantity);
-		Task<bool> RemoveItem(int bookId);
-		Task<IEnumerable<ShoppingCart>> GetUserCart();
+		Task<int> AddItem(int bookId, int quantity);
+		Task<int> RemoveItem(int bookId);
+		Task<ShoppingCart> GetUserCart();
+		Task<int> GetCartItemCount(string userId = "");
+		Task<ShoppingCart> GetCart(string userId);
 	}
 }
